630d6bab6eb13cdb3fd7e3780bca00b41d34faa0
idan@il.ibm.com <idan@il.ibm.com>
2016-01-17 16:11:03 +0200

    added authentication

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,79 @@
 var express = require('express');
+var jwt    = require('jsonwebtoken');
+var config = require('../config/config.json');
 var router = express.Router();
 var Post = require("../models/post");
+var User = require("../models/user");
+
+/* auth */
+
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+router.post('/authenticate', function(req, res) {
+
+  // find the user
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+
+    if (err) {
+      throw err;
+    }
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed.' });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed.' });
+      } else {
+
+        // if user is found and password is right
+        // create a token
+        var token = jwt.sign(user, config.secret, {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+
+    }
+
+  });
+});
+
+// route middleware to verify a token
+var auth = function(req, res, next) {
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+    // verifies secret and checks exp
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+        success: false,
+        message: 'No token provided.'
+    });
+  }
+}
+
 
 /* posts */
 router.route("/posts")
@@ -23,5 +96,32 @@ router.route("/posts")
            res.json(posts);
        });
     });
+
+// route to return all users (GET http://localhost:8080/api/users)
+router.get('/users', auth, function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});
+
+
+/* setup */
+router.get('/setup', function(req, res) {
+
+  // create a sample user
+  var admin = new User({
+    name: 'admin',
+    password: 'admin',
+    admin: true
+  });
+
+  // save the sample user
+  admin.save(function(err) {
+    if (err) throw err;
+
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
 var bodyParser = require('body-parser');
-var port = 8080;
+var morgan = require("morgan");
+var config = require('./config/config.json');
+var port = 8088;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 
 var router = require("./routes/routes");
@@ -13,7 +16,7 @@ app.use('/', router);
 
 var Post = require("./models/post");
 
-mongoose.connect("mongodb://localhost:27017/ginge");
+mongoose.connect(config.database);
 
 app.listen(port);
 console.log("server running on port " + port);
